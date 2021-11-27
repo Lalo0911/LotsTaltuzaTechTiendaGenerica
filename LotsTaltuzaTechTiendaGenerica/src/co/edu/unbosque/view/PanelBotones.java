ecaa85984df9fbcbeedcc11eaaa75760b3ae9a76
DELL <DELL@DESKTOP-QDP1O3S>
2021-11-26 20:26:27 -0500

    Version 0.1

--- LotsTaltuzaTechTiendaGenerica/src/co/edu/unbosque/view/PanelBotones.java
+++ LotsTaltuzaTechTiendaGenerica/src/co/edu/unbosque/view/PanelBotones.java
@@ -5,73 +5,50 @@ import java.awt.*;
 import javax.swing.*;
 
 public class PanelBotones extends JPanel{
-	
-	private static final long serialVersionUID = 1L;
+    
+    private static final long serialVersionUID = 1L;
 
-	private JButton butJugador;
-	private JButton butJuego;
-	private JButton butPartida;
-	private JLabel labTitulo1, labTitulo2;
-	public static final String JUGADOR = "Jugador";
-	public static final String JUEGO = "Juego";
-	public static final String PARTIDA = "Partida";
+    private JButton butClientes;
+    private JButton butProveedores;
+    private JButton butProductos;
+    private JButton butVentas;
+    
+    
+    private JLabel labTitulo1;
+    public static final String CLIENTE = "Cliente";
+    public static final String PROVEEDORES = "Proveedores";
+    public static final String VENTAS = "Venta";
+    public static final String PRODUCTOS = "Productos";
 
-	public PanelBotones() {
-		setLayout( new GridLayout(5,1) );
-		labTitulo1 = new JLabel("LOTS TALTUZA TECH", SwingConstants.CENTER);
-		labTitulo1.setFont((new Font("Bahnschrift",Font.BOLD,20)));
-		add(labTitulo1);
-		butJugador = new JButton("Jugador");
-		butJugador.setActionCommand(JUGADOR);
-		add(butJugador);
-		butJuego = new JButton("Juego");
-		butJuego.setActionCommand(JUEGO);
-		add(butJuego);
-		butPartida = new JButton("Partida");
-		butPartida.setActionCommand(PARTIDA);
-		add(butPartida);
-		labTitulo2 = new JLabel("");
-		add(labTitulo2);
-	}
+    public PanelBotones() {
+        
+        setLayout( new GridLayout(6,1) );
+        
+        labTitulo1 = new JLabel("Nombre Tienda", SwingConstants.CENTER);
+        labTitulo1.setFont((new Font("Bahnschrift",Font.BOLD,20)));
+        add(labTitulo1);
+        
+        butClientes = new JButton("Gestion clientes");
+        butClientes.setActionCommand(CLIENTE);
+        add(butClientes);
+        
+        butProveedores = new JButton("Gestion Proveedores");
+        butProveedores.setActionCommand(PROVEEDORES);
+        add(butProveedores);
+        
+        butProductos = new JButton("Gestion Productos");
+        butProductos.setActionCommand(PRODUCTOS);
+        add(butProductos);
+        
+        butVentas = new JButton("Gestion Ventas");
+        butVentas.setActionCommand(VENTAS);
+        add(butVentas);
+        
+        
+    }
 
-	public JButton getButJugador() {
-		return butJugador;
-	}
-
-	public void setButJugador(JButton butJugador) {
-		this.butJugador = butJugador;
-	}
-
-	public JButton getButJuego() {
-		return butJuego;
-	}
-
-	public void setButJuego(JButton butJuego) {
-		this.butJuego = butJuego;
-	}
-
-	public JButton getButPartida() {
-		return butPartida;
-	}
-
-	public void setButPartida(JButton butPartida) {
-		this.butPartida = butPartida;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-	public static String getJugador() {
-		return JUGADOR;
-	}
-
-	public static String getJuego() {
-		return JUEGO;
-	}
-
-	public static String getPartida() {
-		return PARTIDA;
-	}
-	
-}
+    
+    
+    public JButton getButClientes() {
+        return butClientes;
+    }
